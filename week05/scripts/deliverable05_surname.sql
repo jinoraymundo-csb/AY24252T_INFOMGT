8c7c3695b0dd9243d3cfef7ada039b152a7775c9
jinoraymundo-csb <57472947+jinoraymundo-csb@users.noreply.github.com>
2025-02-10 08:00:58 +0800

    Create deliverable05_surname.sql

---
+++ week05/scripts/deliverable05_surname.sql
@@ -0,0 +1,19 @@
+-- #1 Get email addresses from all customers whose familyname starts with "B"
+
+-- #2 Get given names of all customers who has exactly 4 characters in their given name
+
+-- #3 Get given names and family names of all customers who has "Drive" in their street
+
+-- #4 Get the birthdates (dob) of all customers where their email address ends with ".net"
+
+-- #5 Get all details from all customers where familyname is exactly 5 characters and ends with "s"
+
+-- #6 Get email addresses from all customers where their address contains any of the words "green", "red", and "blue"
+
+-- #7 Get all details from all customers where their familyname has an "'" (apostrophe) and their given name is equal to or more than 5 characters.
+
+-- #8 Get all email addressess from all customers where they don't have the word "street" in their street address, and their familyname contains "ing"
+
+-- #9 Get all street addresses from all customers where their postcode starts with 3, email address ending in ".com" and given name starts with "T"
+
+-- #10 Get all customer details whose birthdate (dob) is on September, givenname less than 6 characters, and are male.
