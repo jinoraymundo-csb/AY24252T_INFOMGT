5aa2a79fabaf2cc9598b580fa0e1e9ee5673e20f
Jino Raymundo <jino.raymundo@benilde.edu.ph>
2025-02-17 07:32:43 +0800

    added deliverable 06

---
+++ week06/02_deliverable_06.md
@@ -0,0 +1,10 @@
+# Deliverable 06: Ordering Results
+
+## A. What to submit
+
+1. Download the `week06\scripts\deliverable06_surname.sql` file from this repo
+2. On SSMS, Click on File -> Open -> File...
+3. Locate the downloaded SQL script file from #1
+4. On this query window, fill in your answers for each question
+5. Once finished, save and rename this file to reflect your actual surname
+6. Submit to Deliverable 05 Dropbox
---
+++ week06/scripts/deliverable_06_surname.sql
@@ -0,0 +1,11 @@
+-- From your deliverable #3 submission:
+
+-- #1 Sort them by name/title in descending order
+
+-- #2 Sort them by name/title in alphabetical order, split them into 4 pages then take the 2nd page
+
+-- #3 Sort them by name/title in reverse alphabetical order, split them into 3 pages then get the last page
+
+-- #4 Rank them on your own and order them via `CHARINDEX()`
+
+-- #5 Rank them on your own ordering via `CHARINDEX()`, split them to two pages, and get the 2nd page
